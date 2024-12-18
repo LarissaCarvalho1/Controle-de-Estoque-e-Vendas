aa290cfdd8e646382b0ed60219079ff21d831b03
Larissa Carvalho <carvalholarissa530@gmail.com>
2024-12-18 01:09:54 -0300

    Projeto inicial completo

--- controle_de_estoque.py
+++ controle_de_estoque.py
@@ -15,10 +15,10 @@ Desculpe, a operação selecionada não é válida.
 Tente novamente!
 """
 # Base de dados
-estoque = [{'nome_produto': 'Celular', 'preco': 1500.00, 'quantidade': 150}]
+estoque = [{'nome_produto': 'Celular', 'preco': 1500.00, 'quantidade': 10}]
 vendas = []
 
-# Ponto de entrada no sistema
+# Inicialização do sistema
 def main():
     while True:
         titulo('CONTROLE DE ESTOQUE')
@@ -47,9 +47,9 @@ def operacao_selecionada(operacao):
         elif operacao == '5':
             registrar_venda()
         elif operacao == '6':
-            print('Você clicou em Visualizar Vendas')
+            visualizar_vendas()
         else:
-            print('Desculpe, a operação selecionada não é válida. Tente novamente!')
+            print(ERRO)
 
 def adicionar_produto():
     titulo('ADICIONAR NOVO PRODUTO')
@@ -58,31 +58,28 @@ def adicionar_produto():
 
         for produto in estoque:
             if produto['nome_produto'] == nome_produto:
-                print('\nProduto já cadastrado no sistema! Utilize a opção de atualizar.\n')
+                print('\nProduto já cadastrado no sistema! \nUtilize a opção ATUALIZAR PRODUTO.\n')
                 return
-        #Solicita as informações de preco e quantidade
         try:
             preco = float(input('Preço do Produto: '))
             quantidade = int(input('Quantidade em Estoque: '))
         except ValueError:
             print('\nERRO: Digite valores numéricos válidos para preço e quantidade.\n')
             continue
-        # Cria e add novo produto ao estoque
         novo_produto = {
             'nome_produto': nome_produto, 
             'preco': preco, 
             'quantidade': quantidade
         }
         estoque.append(novo_produto)
-        print(f'\nProduto: {nome_produto} adicionado com sucesso!\n')
+        print(f'\nProduto: {nome_produto} ADICIONADO com SUCESSO!\n')
 
         while True:
-            resposta = str(input('Deseja cadastrar mais produtos? [S/N] ')).upper().strip()
+            resposta = str(input('Cadastrar outro produto? [S/N] ')).upper().strip()
             if resposta in ['S', 'N']:
                 break
             else:
                 print('\nERRO: Escolha apenas [S] ou [N] para prosseguir.\n')
-        
         if resposta == 'N':
             break
 
@@ -96,28 +93,25 @@ def atualizar_produto():
             if produto['nome_produto'] == nome:
                 produto_encontrado = produto
                 break
-
         if not produto_encontrado:
             print(f'\nProduto {nome} NÃO encontrado no estoque!\n')
             break
-
         try:
             novo_preco = float(input('Novo Preço: '))
             nova_quantidade = int(input('Nova Quantidade: '))
             produto_encontrado['preco'] = novo_preco
             produto_encontrado['quantidade'] = nova_quantidade
-            print(f'\nProduto: {nome} atualizado com SUCESSO!\n')
+            print(f'\nProduto: {nome} ATUALIZADO com SUCESSO!\n')
         except ValueError:
             print('\nERRO: Preço e Quantidade devem ser valores numéricos\n')
             continue
 
         while True:
-            resposta = str(input('Deseja atualizar mais produtos? [S/N] ')).strip().upper()
+            resposta = str(input('Atualizar outro produto? [S/N] ')).strip().upper()
             if resposta in ['S', 'N']:
                 break
             else:
                 print('\nERRO: Escolha apenas [S] ou [N] para prosseguir.\n')
-        
         if resposta == 'N':
             break
     
@@ -129,19 +123,17 @@ def excluir_produto():
         for produto in estoque:
             if produto['nome_produto'] == nome:
                 estoque.remove(produto)
-                print(f'\nProduto {nome} removido com sucesso!\n')
+                print(f'\nProduto {nome} REMOVIDO com SUCESSO!\n')
                 break
         else:
             print('\nProduto não encontrado!\n')
             return
-
         while True:
-            resposta = str(input('Deseja excluir mais itens? [S/N] ')).strip().upper()
+            resposta = str(input('Excluir outro item? [S/N] ')).strip().upper()
             if resposta in ['S', 'N']:
                 break
             elif resposta != 'S':
                 print('\nERRO: Escolha apenas [S] ou [N] para prosseguir.\n')
-
         if resposta == 'N':
             print('\nEncerrando a exclusão de produtos...\n')
             break
@@ -156,7 +148,89 @@ def visualizar_estoque():
             print(f'Preço: {produto['preco']:.2f}')
             print(f'Quantidade: {produto['quantidade']}')
             print('--'*26)
+    aguardar_enter()
 
+def registrar_venda():
+    titulo('REGISTRAR VENDA')
+    while True:
+        nome_cliente = str(input('Nome do Cliente: ')).strip().capitalize()
+        nome_produto_venda = str(input('Produto: ')).strip().capitalize()
+        produto_encontrado = None
+
+        # Verifica se o nome_produto_venda digitado pelo usuário faz parte da lista estoque
+        for produto in estoque:
+            if produto['nome_produto'] == nome_produto_venda:
+                produto_encontrado = produto
+                break
+        if not produto_encontrado:
+            print(f'Produto: {nome_produto_venda} NÃO encontrado no estoque!')
+            break
+        if produto_encontrado['quantidade'] == 0:
+            print(f'Produto {produto_encontrado["nome_produto"]} está sem estoque!')
+            break
+
+        print(f'Preço: R$ {produto_encontrado["preco"]:.2f}')
+        print(f'Estoque disponível: {produto_encontrado["quantidade"]}')
+
+        # Valida quantidade de produtos comprados
+        while True:
+            try:
+                quantidade_compra = int(input('Quantidade de produtos comprados: '))
+                if quantidade_compra <= 0:
+                    print('ERRO: A quantidade deve ser maior que zero.')
+                elif quantidade_compra > produto_encontrado['quantidade']:
+                    print('ERRO: A quantidade solicitada excede o estoque disponével.')
+                else:
+                    break
+            except ValueError:
+                print('ERRO: Insira um número válido para a quantidade.')
+                continue
+        
+        # Confirma compra
+        while True:
+            continuar = str(input('Finalizar Compra? [S/N]: ')).strip().upper()
+            if continuar in ['S', 'N']:
+                break
+            else: 
+                print('\nERRO: Escolha apenas [S] ou [N] para prosseguir.\n')
+        
+        # Cria nova venda e adiciona à lista vendas
+        if continuar == 'S':
+            produto_encontrado['quantidade'] -= quantidade_compra
+            total_compra = produto_encontrado['preco'] * quantidade_compra
+            nova_venda = {
+                'cliente': nome_cliente,
+                'produto': produto_encontrado['nome_produto'],
+                'preco': produto_encontrado['preco'],
+                'quantidade': quantidade_compra,
+                'total': total_compra
+            }
+            vendas.append(nova_venda)
+            print('Compra realizada com SUCESSO!')
+        else:
+            print('\nCompra cancelada!')
+        break
+
+def visualizar_vendas():
+    titulo('VENDAS REALIZADAS')
+    if not vendas:
+        print('Nenhuma venda realizada!')
+    else:
+        for venda in vendas:
+            print(f'Cliente: {venda['cliente']}')
+            print(f'Produto: {venda['produto']}')
+            print(f'Preço: {venda['preco']:.2f}')
+            print(f'Quantidade: {venda['quantidade']}')
+            print(f'Total: {venda['total']:.2f}')
+            print('--'*26)
+    aguardar_enter()
+
+def titulo(txt):
+    print('--'*26)
+    print(f"|{txt:^50}|")
+    print('--'*26)
+
+def aguardar_enter():
     while True:
         resposta = str(input('Pressione Enter para voltar ao menu. ')).strip()
         if resposta == '':
@@ -164,37 +238,4 @@ def visualizar_estoque():
         else:
             print('\nERRO: Não digite nada, apenas pressione Enter.\n')
 
-def registrar_venda():
-    titulo('REGISTRAR VENDA')
-    while True:
-        nome_cliente = str(input('Nome do Cliente: ')).strip()
-        nome_produto_venda = str(input('Produto: ')).strip().capitalize()
-        produto_encontrado = None
-
-        for produto in estoque:
-            if produto['nome_produto'] == nome_produto_venda:
-                produto_encontrado = produto
-                break
-
-        if not produto_encontrado:
-            print(f'Produto: {nome_produto_venda} NÃO encontrado no estoque!')
-            break
-
-        if produto_encontrado['quantidade'] != 0:
-            print(f'No estoque há {produto_encontrado['quantidade']} produto {produto_encontrado['nome_produto']} ')
-            break
-
-
-
-def visualizar_vendas():
-    print('Visualizar vendas')
-
-def titulo(txt):
-    print('--'*26)
-    print(f"|{txt:^50}|")
-    print('--'*26)
-
 main()
-
-
-
